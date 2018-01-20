6faaf43bf271f03a929890aa04675981e7b04ea5
Robert Westerlund <robert.westerlund@live.com>
2018-01-20 12:38:13 +0100

    Added additional logging statements

--- src/rpi-app/app.ts
+++ src/rpi-app/app.ts
@@ -4,6 +4,8 @@ import * as process from 'process'
 import * as csv from 'csv-string'
 import { MessageEnqueued } from 'azure-iot-common/lib/results';
 
+log("Catbell application starting");
+
 const iotHubConnectionstringEnvironmentVariableName = 'IOT_HUB_CONNECTIONSTRING';
 
 const iotHubConnectionstring = process.env[iotHubConnectionstringEnvironmentVariableName];
@@ -37,7 +39,7 @@ function onIotHubConnected() {
 }
 
 function createLoggingHandlerFor(op: string) {
-    return function printResult(err?: Error, res?: any) {
+    return function logResult(err?: Error, res?: any) {
         if (err) log(op + ' error: ' + err.toString(), 'ERROR');
         if (res) log(op + ' status: ' + res.constructor.name);
     };
@@ -52,6 +54,7 @@ function sendProximityInfo(isProximityDetected: boolean, callback?: (err?: Error
     };
     var message = new Message(JSON.stringify(data));
     message.properties.add('MessageType', messageType);
+    log({message: "Sending proximity info", data: data});
     if (!callback){
         callback = createLoggingHandlerFor('sendProximityInfo');
     }
